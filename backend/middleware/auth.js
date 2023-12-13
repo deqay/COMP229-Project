283cdf529c63bb8e8b690d974c71dbbebe42654f
harpsinghh <harpreetsinghh1984@gmail.com>
2023-12-13 02:32:32 -0500

    project commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,23 +3,30 @@ const catchAsyncErrors = require("./catchAsyncError");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+const extractToken = (rawToken) => {
+  // Check if rawToken is an array
+  if (!Array.isArray(rawToken)) {
+    return null;
+  }
+
+  const cookiesIndex = rawToken.indexOf("Cookies");
+
+  if (cookiesIndex !== -1 && cookiesIndex + 1 < rawToken.length) {
+    const cookiesValue = rawToken[cookiesIndex + 1];
+    const match = cookiesValue.match(/token=([^;]+)/);
+
+    if (match) {
+      return match[1];
+    }
+  }
+
+  return null;
+};
+
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const rawToken = req.rawHeaders;
   const token = extractToken(rawToken);
-  const extractToken = (rawToken) => {
-    const cookiesIndex = rawToken.indexOf("Cookies");
 
-    if (cookiesIndex !== -1 && cookiesIndex + 1 < rawToken.length) {
-      const cookiesValue = rawToken[cookiesIndex + 1];
-      const match = cookiesValue.match(/token=([^;]+)/);
-
-      if (match) {
-        return match[1];
-      }
-    }
-
-    return null;
-  };
   console.log(token);
   if (!token) {
     return next(new ErrorHandler("Please login to update the profile", 401));
