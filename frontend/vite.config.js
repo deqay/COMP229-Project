5d91c7e0d2b02801a1c2708a90d9fcbef8442a79
harpsinghh <harpreetsinghh1984@gmail.com>
2023-12-12 19:40:23 -0500

    project commit-5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '.',
- 
-})
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
+  base: ".",
+});
